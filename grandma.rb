9005e11482196850031fe905390d8da1188963cc
Ashna Shah-Grover <as19@williams.edu>
2019-05-09 20:16:54 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(input)
-  if input == input.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif input == "I LOVE YOU GRANDMA!"
+  if input == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif input == input.upcase
+    return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
+  end
 end
